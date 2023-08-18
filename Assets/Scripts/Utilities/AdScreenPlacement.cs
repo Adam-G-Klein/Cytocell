c42da26a5193e6521ae1e84874ee830ad9c7ba25
agklein <agklein@amazon.com>
2023-08-18 15:27:40 -0700

    WIP, banner placed wrong

---
+++ Assets/Scripts/Utilities/AdScreenPlacement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdScreenPlacement : MonoBehaviour {
+
+    public static int[] topBannerPos(){
+        return new int[]{(int)(Screen.safeArea.xMax + Screen.safeArea.xMin)/2, (int)Screen.safeArea.yMin};
+    }
+}
