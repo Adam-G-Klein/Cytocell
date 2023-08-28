87f1b9dbd2086241a091bbf027a956e59e013995
agklein <agklein@amazon.com>
2023-08-28 08:32:12 -0700

    Ads work, placement WIP

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowBannerWhenReady());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator ShowBannerWhenReady()
+    {
+        /*
+        while (!Advertisement.IsReady("Banner"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show("Banner");
+        */
+    }
+}
