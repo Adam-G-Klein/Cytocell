e031b99837d5c04ca2a2b00a6bc9be3440dfb084
agklein <agklein@amazon.com>
2023-10-11 19:46:23 -0700

    shopStuff

---
+++ Assets/Scripts/UI/ShopScripts/ShopItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Each display needs: 
+- a demo player
+- a price display
+- a buy button
+- a background
+
+*/
+public class ShopItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
