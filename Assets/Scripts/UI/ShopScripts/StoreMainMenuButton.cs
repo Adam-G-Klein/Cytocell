e02e33514c5088c56026a1cd1e5d2a752cc28e25
agklein <agklein@amazon.com>
2023-10-11 21:26:48 -0700

    Store menu set up

---
+++ Assets/Scripts/UI/ShopScripts/StoreMainMenuButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Clickable))]
+public class StoreMainMenuButton : MonoBehaviour
+{
+    public GameObject storeMenuWrapper;
+    private ButtonGroupAlphaControls storeMenuButtons;
+    public GameObject mainMenuWrapper;
+
+    private ButtonGroupAlphaControls mainMenuButtons;
+    private Clickable clickable;
+
+    void Start()
+    {
+        clickable = GetComponent<Clickable>();
+        storeMenuButtons = storeMenuWrapper.GetComponent<ButtonGroupAlphaControls>();
+        mainMenuButtons = mainMenuWrapper.GetComponent<ButtonGroupAlphaControls>();
+    }
+
+    public void onClick()
+    {
+        print("store onclick");
+        if (clickable.clickable)
+        {
+            storeMenuButtons.displayAll();
+            mainMenuButtons.hideAll();
+            print("displaying store menu");
+        }
+    }
+
+}
